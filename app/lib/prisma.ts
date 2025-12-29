b2d4458dc4862cdcab19c27b406071b3f2ff1c40
volkan <vlkngms7@gmail.com>
2025-12-29 21:41:12 +0300

    sa

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -8,6 +8,9 @@ declare global {
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL!,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 
 const adapter = new PrismaPg(pool);
