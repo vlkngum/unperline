db9f5bb2ec7a405062b158822ec61c2db8cd02b7
volkan <vlkngms7@gmail.com>
2025-12-30 00:30:30 +0300

    buid4

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,21 +1,14 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "pg";
 
 declare global {
     var prisma: PrismaClient | undefined;
 }
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL!,
-    ssl: {
-        rejectUnauthorized: false,
-    },
-});
-
-const adapter = new PrismaPg(pool);
-
-export const prisma = global.prisma ?? new PrismaClient({ adapter });
+export const prisma =
+    global.prisma ??
+    new PrismaClient({
+        log: ["error"],
+    });
 
 if (process.env.NODE_ENV !== "production") {
     global.prisma = prisma;
