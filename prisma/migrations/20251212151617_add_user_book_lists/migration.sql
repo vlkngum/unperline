49e1020e98ee7415e05b8454aa2a66d22558ae5e
volkan <vlkngms7@gmail.com>
2025-12-30 00:11:21 +0300

    buid

--- prisma/migrations/20251212151617_add_user_book_lists/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "likedBooks" TEXT[] DEFAULT ARRAY[]::TEXT[],
-ADD COLUMN     "readBooks" TEXT[] DEFAULT ARRAY[]::TEXT[],
-ADD COLUMN     "watchlist" TEXT[] DEFAULT ARRAY[]::TEXT[];
--- prisma/migrations/20251215162943_user/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "avatarUrl" TEXT,
-ADD COLUMN     "bannerUrl" TEXT,
-ADD COLUMN     "bio" TEXT,
-ADD COLUMN     "favoriteBooks" TEXT[] DEFAULT ARRAY[]::TEXT[],
-ADD COLUMN     "fullName" TEXT,
-ADD COLUMN     "location" TEXT,
-ADD COLUMN     "pronouns" TEXT;
