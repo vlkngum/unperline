7cf10a5f2ea4652717f71a9ce57a0a09adf32606
volkan <vlkngms7@gmail.com>
2026-02-07 03:21:03 +0300

    push

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,12 +1,20 @@
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 declare global {
     var prisma: PrismaClient | undefined;
 }
 
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
 export const prisma =
     global.prisma ??
     new PrismaClient({
+        adapter,
         log: ["error"],
     });
 
